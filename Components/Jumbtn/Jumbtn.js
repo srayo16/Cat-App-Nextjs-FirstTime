bfd6f2db6a0b352484af83b7aa257431981eb4da
srayo16 <srayosikder@gmail.com>
2022-06-18 23:43:19 +0600

    branch wanted

---
+++ Components/Jumbtn/Jumbtn.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Jumbtn = () => {
+    return (
+
+        <div className="jumbotron jumbotron-fluid mt-5">
+            <div className="container">
+                <h1 className="display-4">Here your kitty cat</h1>
+                <p className="lead">Please select your kitty cat from here and adopt.</p>
+                <button type="button" className="btn btn-primary">Go to checkout</button>
+            </div>
+        </div>
+    );
+};
+
+export default Jumbtn;
---
+++ pages/cats.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const cats = () => {
+    return (
+        <div>
+            <h1>This is your cats</h1>
+        </div>
+    );
+};
+
+export default cats;
